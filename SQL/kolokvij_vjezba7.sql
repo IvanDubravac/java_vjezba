a9b827d37c4dd94cf8f46e58dc28ec4029f30cf2
IvanDubravac <ivandubravac99@gmail.com>
2022-12-07 15:50:31 +0100

    ispravak 7.

--- SQL/kolokvij_vjezba7.sql
+++ SQL/kolokvij_vjezba7.sql
@@ -79,3 +79,31 @@ alter table muskarac_decko add foreign key(muskarac) references muskarac(sifra);
 alter table muskarac_decko add foreign key(decko) references decko(sifra);
 
 
+
+ 
+
+
+insert into decko (treciputa,ogrlica,ekstrovertno)
+values(2022,15,1),(1999,2323,1),(1987,66565,0);
+
+insert into muskarac (drugiputa)
+values(1895),(2005),(2000);
+
+insert into muskarac_decko (muskarac,decko)
+values(1,1),(3,3),
+(2,2);
+
+insert into becar(eura,muskarac )
+values(152,1),(152,2),(152,3);
+
+insert into neprijatelj (ogrlica,becar)
+values(152,1),(232323,2),(54545,3);
+
+update cura set indiferentno = false 
+where sifra is not null ;
+
+delete from brat 
+where novcica != 12.75;
+
+select prviputa  from becar 
+where treciputa is null ;
