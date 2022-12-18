2196837ea847187caec086db7d5b56e8a66a8746
IvanDubravac <ivandubravac99@gmail.com>
2022-12-18 23:03:02 +0100

    zbroj znamenki u broju

--- eclipse/edunova/src/edunova/Z04.java
+++ eclipse/edunova/src/edunova/Z04.java
@@ -6,12 +6,16 @@ public class Z04 {
 
 	public static void main(String[] args) {
 
-		int i = Integer.parseInt(JOptionPane.showInputDialog("Unesite dvoznamenkasti broj: "));
+		int broj = Integer.parseInt(JOptionPane.showInputDialog("Unesite broj kojemu želite zbrojiti znamenke: "));
 
-		 int r = i % 10;
-		
-		System.out.println(r);
+		int r = 0, znamenka;
 
+		while (broj > 0) {
+			znamenka = broj % 10;
+			r = r + znamenka;
+			broj = broj / 10;
+		}
+			System.out.println(r);
 	}
 
 }
