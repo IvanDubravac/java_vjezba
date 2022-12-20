d95a5b01231675387156e5626c3255d1b7c39bda
IvanDubravac <ivandubravac99@gmail.com>
2022-12-20 20:00:50 +0100

    if-grananje

---
+++ eclipse/edunova/src/edunova03/E0105.java
@@ -0,0 +1,32 @@
+package edunova03;
+
+public class E0105 {
+	
+	public static void main(String[] args) {
+		
+		boolean uvjet=true;
+		
+		if(uvjet) {
+			System.out.println("1");
+		}else {
+			System.out.println("2");
+		}
+		
+		int i=7;
+		if(i<10) {
+			System.out.println("3");
+		}
+		
+		
+		int j=3;
+		
+		if(j==7) {
+			System.out.println("1");
+		}else if(j==5) {
+			System.out.println("2");
+		}else {
+			System.out.println("Ništa");
+		}
+	}
+
+}
