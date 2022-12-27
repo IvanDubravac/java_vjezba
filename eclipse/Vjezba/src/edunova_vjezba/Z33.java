336691ae231a45f17206073c6617667f460bc4ea
IvanDubravac <ivandubravac99@gmail.com>
2022-12-27 23:13:42 +0100

    zadatak-prosti brojevi

---
+++ eclipse/Vjezba/src/edunova_vjezba/Z33.java
@@ -0,0 +1,33 @@
+package edunova_vjezba;
+
+import javax.swing.JOptionPane;
+
+public class Z33 {
+	public static void main(String[] args) {
+
+		int broj1 = Integer.parseInt(JOptionPane.showInputDialog("Unesite prvi broj"));
+		int broj2 = Integer.parseInt(JOptionPane.showInputDialog("Unesite drugi broj"));
+		int min, max;
+		if (broj1 < broj2) {
+			min = broj1;
+			max = broj2;
+		} else {
+			min = broj2;
+			max = broj1;
+		}
+		for (int i = min; i < max; i++) {
+			int brojac = 0;
+			for (int j = 2; j < i; j++) {
+				if (i % j == 0) {
+					brojac++;
+
+				}
+
+			}
+			if (brojac == 0) {
+				System.out.println(i);
+			}
+
+		}
+	}
+}
