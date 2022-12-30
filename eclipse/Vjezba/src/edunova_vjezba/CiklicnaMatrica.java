ee9c5f11532ccddf68fec96dae6d1e6e68b6e6b0
IvanDubravac <ivandubravac99@gmail.com>
2022-12-30 20:07:17 +0100

    ciklicnaMatrica-prvi stupac

--- eclipse/Vjezba/src/edunova_vjezba/CiklicnaMatrica.java
+++ eclipse/Vjezba/src/edunova_vjezba/CiklicnaMatrica.java
@@ -7,23 +7,20 @@ public class CiklicnaMatrica {
 		int redak = Integer.parseInt(JOptionPane.showInputDialog("Unesite broj redaka"));
 		int stupac = Integer.parseInt(JOptionPane.showInputDialog("Unesite broj stupaca"));
 		int matrica[][] = new int[redak][stupac];
+		int zadnji=0;
+		for (int a = 1; a <= stupac; a++) {
+			matrica[redak - 1][stupac - a] = a;
+			zadnji=a;
+
+		}
+		for (int i = redak-1; i >=0; i--) {
+				matrica[i][0] = zadnji++;
+		}
 		for (int i = 0; i < redak; i++) {
 			for (int j = 0; j < stupac; j++) {
 				System.out.print(matrica[i][j] + " ");
 
 			}
-			
-			for(int z=0; z<=redak-1; z++) {
-				for (int b = redak + stupac - 1; b <= redak - 1; b--) {
-					matrica[redak-z][0]=b;
-			}
-			}
-			for (int a = 1; a <= stupac; a++) {
-				matrica[redak - 1][stupac - a] = a;
-
-			}
-		
-			
 			System.out.println();
 		}
 
