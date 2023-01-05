5817f48f945f60e7422e40759e90e2533907d72a
IvanDubravac <ivandubravac99@gmail.com>
2023-01-05 13:33:13 +0100

    vjezba

--- eclipse/Vjezba/src/edunova_vjezba/Z_Ponavljanje_24broja.java
+++ eclipse/Vjezba/src/edunova_vjezba/Z_Ponavljanje_24broja.java
@@ -2,31 +2,31 @@ package edunova_vjezba;
 
 import javax.swing.JOptionPane;
 
-public class Zad11 {
+public class Z_Ponavljanje_24broja {
 	public static void main(String[] args) {
-		int polje[] = new int[50];
+		int polje[] = new int[24];
 		int najmanji = 0;
 		int najveci = 0;
 		int zbroj = 0;
 		for (int i = 0; i < 3; i++) {
-			int broj = Integer.parseInt(JOptionPane.showInputDialog("Unesi" + " " + (i + 1) + "." + " broj "));
+			int broj = Integer.parseInt(JOptionPane.showInputDialog("Unesite" + " " + (i + 1) + ". " + "broj"));
 			polje[i] = broj;
-			zbroj = zbroj + polje[i];
-			najmanji = polje[0];
-			najveci = polje[0];
+			zbroj += broj;
+			najmanji=polje[0];
+			najveci=polje[0];
+
 			if (polje[i] < polje[i + 1]) {
 				najmanji = polje[i];
-
 			}
 
 			if (polje[i] > polje[i + 1]) {
 				najveci = polje[i];
+
 			}
 		}
 		System.out.println(zbroj);
 		System.out.println(najmanji);
 		System.out.println(najveci);
-
 	}
 
 }
