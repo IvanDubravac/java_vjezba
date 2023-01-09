1bfc85e4d5d6aa34fa1a4aa23881715429e03337
IvanDubravac <ivandubravac99@gmail.com>
2023-01-09 21:52:58 +0100

    ciklicna matrica-valja

--- eclipse/Vjezba/src/edunova_vjezba/CiklicnaMatrica.java
+++ eclipse/Vjezba/src/edunova_vjezba/CiklicnaMatrica.java
@@ -4,37 +4,59 @@ import javax.swing.JOptionPane;
 
 public class CiklicnaMatrica {
 	public static void main(String[] args) {
-		int redak = Integer.parseInt(JOptionPane.showInputDialog("Unesite broj redaka"));
+		int redak = Integer.parseInt(JOptionPane.showInputDialog("Unesite broj redaka "));
 		int stupac = Integer.parseInt(JOptionPane.showInputDialog("Unesite broj stupaca"));
+
+		int minR = 0;
+		int maxR = redak - 1;
+		int minS = 0;
+		int maxS = stupac - 1;
+		int br = 1;
+
 		int matrica[][] = new int[redak][stupac];
-		int zadnji = 1;
-			
-		for (int q = 1; q <= stupac; q++) {
-			matrica[redak - 1][stupac - q] = q;
-			zadnji = q;
-		}
 
-		for (int i = redak - 1; i >= 0; i--) {
-			matrica[i][0] = zadnji++;
+		while (true) {
+			if (minS > maxS) {
+				break;
+			}
 
-		}
+			for (int i = maxS; i >= minS; i--) {
+				matrica[maxR][i] = br++;
+			}
+			maxR--;
 
-		for (int a = 1; a < stupac; a++) {
-			matrica[0][a] = zadnji++;
-		}
-			
-		for (int i = 1; i < redak - 1; i++) {
-			matrica[i][stupac - 1] = zadnji++;
+			if (minR > maxR) {
+				break;
+			}
+
+			for (int i = maxR; i >= minR; i--) {
+				matrica[i][minS] = br++;
+			}
+			minS++;
+
+			if (minS > maxS) {
+				break;
+			}
+
+			for (int i = minS; i <= maxS; i++) {
+				matrica[minR][i] = br++;
+			}
+			minR++;
+
+			if (minR > maxR) {
+				break;
+			}
+
+			for (int i = minR; i <= maxR; i++) {
+				matrica[i][maxS] = br++;
+
+			}
+			maxS--;
 		}
 
 		for (int i = 0; i < redak; i++) {
 			for (int j = 0; j < stupac; j++) {
-				if (matrica[i][j] >= 10) {
-					System.out.print(matrica[i][j] + " ");
-				} else {
-					System.out.print(" " + matrica[i][j] + " ");
-				}
-
+				System.out.print(matrica[i][j] + "\t");
 			}
 
 			System.out.println();
