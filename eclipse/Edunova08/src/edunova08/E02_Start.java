42dfbe0f940266da6611c2d011585a93bfda9c2b
IvanDubravac <ivandubravac99@gmail.com>
2023-01-20 00:37:44 +0100

    Update E02_Start.java

--- eclipse/Edunova08/src/edunova08/E02_Start.java
+++ eclipse/Edunova08/src/edunova08/E02_Start.java
@@ -1,5 +1,7 @@
 package edunova08;
 
+import java.util.Date;
+
 public class E02_Start {
 	public static void main(String[] args) {
 		
@@ -21,16 +23,19 @@ public class E02_Start {
 //		}
 //		
 		
-		Mjesto mjesto=new Mjesto();
-		Zupanija zupanija=new Zupanija();
-		zupanija.setNaziv("OBŽ");
-		mjesto.setZupanija(zupanija);
-		System.out.println(mjesto.getZupanija().getNaziv());
+//		Mjesto mjesto=new Mjesto();
+//		Zupanija zupanija=new Zupanija();
+//		zupanija.setNaziv("OBŽ");
+//		mjesto.setZupanija(zupanija);
+//		System.out.println(mjesto.getZupanija().getNaziv());
 		
 		
+		Grupa jp27=kreirajGrupu(1, "JP27", 20, new Date(), kreirajSmjer(1, "Java programiranje", 5999.99f, 500, true));
+		Grupa pp26= kreirajGrupu(2, "PP26", 20, new Date(), kreirajSmjer(1, "PHP programiranje", 4999.99f, 500, false));
 		
-		
-		
+		System.out.println(jp27.getNaziv());
+		System.out.println(pp26.getNaziv());
+		System.out.println(jp27.getSmjer().getCijena()+pp26.getSmjer().getCijena());
 		
 		
 	}
@@ -46,6 +51,28 @@ public class E02_Start {
 	
 	}
 	
+	private static Smjer kreirajSmjer(int sifra, String naziv, float cijena, float upisnina, boolean certificiran) {
+		Smjer s=new Smjer();
+		s.setSifra(sifra);
+		s.setNaziv(naziv);
+		s.setCijena(cijena);
+		s.setUpisnina(upisnina);
+		s.setCertificiran(certificiran);
+		
+		return s;
+		
+	}
 	
+	private static Grupa kreirajGrupu(int sifra, String naziv, int maksimalnoPolaznika, Date datumPocetka, Smjer smjer) {
+		Grupa g=new Grupa();
+		g.setSifra(sifra);
+		g.setNaziv(naziv);
+		g.setMaksimalnoPolaznika(maksimalnoPolaznika);
+		g.setDatumPocetka(datumPocetka);
+		g.setSmjer(smjer);
+		
+		return g;
+	
+	}
 	
 }
