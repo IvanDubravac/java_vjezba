2ea238e7a5d5474bd8d728a290de73645fe3f5f1
IvanDubravac <ivandubravac99@gmail.com>
2023-01-27 19:44:56 +0100

    Ljubavni test
    pocetak

---
+++ eclipse/Vjezba/src/edunova_vjezba/TestLjubavi.java
@@ -0,0 +1,23 @@
+package edunova_vjezba;
+
+import java.util.Arrays;
+
+import javax.swing.JOptionPane;
+
+public class TestLjubavi {
+
+	public static void main(String[] args) {
+
+		String ime1 = JOptionPane.showInputDialog("Unesite prvo ime");
+		String ime2 = JOptionPane.showInputDialog("Unesite drugo ime");
+	
+	}
+
+	public static int[] BrojacSlova(String ime1, String ime2) {
+
+		String zbrojImena = ime1 + ime2;
+		char imenaUPolje[]=(zbrojImena.toUpperCase().toCharArray());
+		
+	}
+
+}
