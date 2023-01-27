544150fe57300cf650e1def48d1705de8c933fb9
IvanDubravac <ivandubravac99@gmail.com>
2023-01-27 21:18:50 +0100

    polje charova u polje intova

--- eclipse/Vjezba/src/edunova_vjezba/TEST.java
+++ eclipse/Vjezba/src/edunova_vjezba/TEST.java
@@ -4,31 +4,29 @@ import java.util.Arrays;
 
 import javax.swing.JOptionPane;
 
-public class TestLjubavi {
-
+public class TEST {
 	public static void main(String[] args) {
 
 		String ime1 = JOptionPane.showInputDialog("Unesite prvo ime");
 		String ime2 = JOptionPane.showInputDialog("Unesite drugo ime");
-			System.out.println(Arrays.toString(BrojacSlova(ime1, ime2)));
-	}
 
-	public static int[] BrojacSlova(String ime1, String ime2) {
 		int brojac;
 		String zbrojImena = ime1 + ime2;
 		char slovaUPolju[] = (zbrojImena.toUpperCase().toCharArray());
 		int poljeBrojeva[] = new int[zbrojImena.length()];
 
-		for (int i = 0; i < (zbrojImena.length() ); i++) {
+		for (int i = 0; i < zbrojImena.length(); i++) {
 			brojac = 0;
-			for (int j = 0; j < (zbrojImena.length() ); j++) {
+			for (int j = 0; j < zbrojImena.length(); j++) {
 				if (slovaUPolju[i] == slovaUPolju[j]) {
 					brojac++;
 				}
 			}
 			poljeBrojeva[i] = brojac;
 		}
-		return poljeBrojeva;
+
+		System.out.println(Arrays.toString(poljeBrojeva));
+
 	}
-	
+
 }
