e8b52ae747fc73b2e8e2884e69b322bea781437f
IvanDubravac <ivandubravac99@gmail.com>
2023-01-28 19:26:03 +0100

    Update TestLjubavi.java

--- eclipse/Vjezba/src/edunova_vjezba/TestLjubavi.java
+++ eclipse/Vjezba/src/edunova_vjezba/TestLjubavi.java
@@ -13,7 +13,7 @@ public class TestLjubavi {
 
 	}
 
-	public static int[] BrojacSlova(String ime1, String ime2) {
+	public static int[] prviKorak(String ime1, String ime2) {
 		int brojac;
 		String zbrojImena = ime1 + ime2;
 		char slovaUPolju[] = (zbrojImena.toUpperCase().toCharArray());
@@ -28,22 +28,53 @@ public class TestLjubavi {
 			}
 			poljeBrojeva[i] = brojac;
 		}
-		int meduPolje[] = new int[ime2.length()];
+		int veci = 0, manji = 0;
 		if (ime1.length() > ime2.length()) {
-			for (int i = 0; i < ime2.length(); i++) {
+			veci = ime1.length();
+			manji = ime2.length();
+		} else {
+			manji = ime1.length();
+			veci = ime2.length();
+		}
+
+		int meduPolje[] = new int[veci];
+		if (veci > manji) {
+			for (int i = 0; i < manji; i++) {
 				meduPolje[i] = poljeBrojeva[i] + poljeBrojeva[poljeBrojeva.length - i - 1];
 			}
-			for (int i = 0; i < ( ime1.length() - ime2.length()); i++) {
-				meduPolje[ime2.length() + i] = poljeBrojeva[ime2.length() + i];
+			for (int i = 0; i < (veci - manji); i++) {
+				meduPolje[manji + i] = poljeBrojeva[manji + i];
 			}
 		} else {
-			for (int i = 0; i < ime1.length(); i++) {
+			for (int i = 0; i < manji; i++) {
 				meduPolje[i] = poljeBrojeva[i] + poljeBrojeva[poljeBrojeva.length - i - 1];
 			}
-			for (int i = 0; i < (ime2.length() - ime1.length()); i++) {
-				meduPolje[ime1.length() + i] = poljeBrojeva[ime1.length() + i];
+			for (int i = 0; i < (veci - manji); i++) {
+				meduPolje[manji + i] = poljeBrojeva[manji + i];
 			}
 		}
 
+		return meduPolje;
+	}
+
+	private static int [] kalkulator(int meduPolje[]) {
+		boolean test=false;
+		int []rjesenje=new int[2];
+		do {
+			int testPrvi = 0;
+			int [] polje = new int[meduPolje.length/2];
+			for(int i=0; i< (meduPolje.length)/2 ; i++) {
+				if(testPrvi==0) {
+					polje[i]=meduPolje[i]+meduPolje[meduPolje.length-1+i];
+					testPrvi++;
+				}else {
+					polje[i]=polje[i]+polje[polje.length-1+i];
+				}
+				
+			}
+			
+		}while(test==false);
+		
+		return rjesenje;
 	}
 }
