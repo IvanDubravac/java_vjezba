c7c5d6f640b54fe13106cbf7724c0d888defc14e
IvanDubravac <ivandubravac99@gmail.com>
2023-02-15 14:35:32 +0100

    Zadatak1

---
+++ netbeans/zadatak/src/main/java/edunova/Zadatak.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edunova;
+
+/**
+ *
+ * @author marko
+ */
+public class Zadatak {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new Prozor1().setVisible(true);
+    }
+}
