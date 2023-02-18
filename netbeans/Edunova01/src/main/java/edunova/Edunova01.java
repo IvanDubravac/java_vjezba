54d65ec9aa1b0e9820949dbb367e1f3a65833bd4
IvanDubravac <ivandubravac99@gmail.com>
2023-02-18 20:28:19 +0100

    spajanje na bazu

--- netbeans/Edunova01/src/main/java/edunova/Edunova01.java
+++ netbeans/Edunova01/src/main/java/edunova/Edunova01.java
@@ -4,21 +4,57 @@
 
 package edunova;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author marko
  */
 public class Edunova01 {
-       String myDriver = "org.mariadb.jdbc.Driver";
-      String myUrl = "jdbc:mariadb://localhost/edunovajp27";
-    
+      private String myDriver = "org.mariadb.jdbc.Driver";
+      private String myUrl = "jdbc:mariadb://localhost/edunovajp27";
+      private Connection conn;
     
     public Edunova01(){
-        
+         spojiSeNaBazu();
+         primjerSelect();
+         
      
     }
 
     public static void main(String[] args) {
         new Edunova01();
+    }
+
+    private void spojiSeNaBazu() {
+          try {
+               Class.forName(myDriver);
+               conn = DriverManager.getConnection(myUrl, "root", "");
+           } catch (ClassNotFoundException ex) {
+               ex.printStackTrace();
+           } catch (SQLException ex) {
+              ex.printStackTrace();
+           }
+    }
+
+    private void primjerSelect() {
+        String query = "select * from smjer";
+          try {
+              Statement st = conn.createStatement();
+              ResultSet rs = st.executeQuery(query);
+              while (rs.next()){
+                  System.out.println(rs.getString("naziv"));
+              
+              }
+
+          } catch (SQLException ex) {
+              ex.printStackTrace();
+          }
     }
 }
