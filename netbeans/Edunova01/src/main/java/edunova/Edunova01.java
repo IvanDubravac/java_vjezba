42e6d2bafc84405a2c4adf28891d672d6da6e1b0
IvanDubravac <ivandubravac99@gmail.com>
2023-02-18 19:39:08 +0100

    Spajanje na bazu

---
+++ netbeans/Edunova01/src/main/java/edunova/Edunova01.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edunova;
+
+/**
+ *
+ * @author marko
+ */
+public class Edunova01 {
+       String myDriver = "org.mariadb.jdbc.Driver";
+      String myUrl = "jdbc:mariadb://localhost/edunovajp27";
+    
+    
+    public Edunova01(){
+        
+     
+    }
+
+    public static void main(String[] args) {
+        new Edunova01();
+    }
+}
